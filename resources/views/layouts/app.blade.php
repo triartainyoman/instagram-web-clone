3bf239e66367ff5313b0b562ad9ac32618b4e5b6
triartainyoman <triarta.art@gmail.com>
2021-10-29 16:52:36 +0800

    Initial project & Setup Tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container px-4">
+        <h2>Hello World!</h2>
+    </div>
+</body>
+
+</html>
